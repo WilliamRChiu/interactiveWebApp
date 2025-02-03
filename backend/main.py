16254d9a6609ff561f0ed1ba7ca4cefaba4fd1da
William Chiu <williamrchiu18@gmail.com>
2025-02-02 21:17:29 -0500

    added structure

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI from Docker on Linux!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/items/")
+async def add_item(name: str, location: int):
+    return{"name":name, "location":location}
